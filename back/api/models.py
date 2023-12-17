b103707a2328e974253052f2e2aeb5b8ab258aed
Viktor Lenard <viktor.lenard@gmail.com>
2023-12-17 17:42:09 +0000

    Adjusted password model

--- back/api/models.py
+++ back/api/models.py
@@ -21,11 +21,11 @@ class Password(models.Model):
         (purple, "purple"),
     ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='user_passwords')
-    name = encrypt(models.CharField(max_length=64)) # Required
-    username = encrypt(models.CharField(max_length=1000, null=True, blank=True))
-    ciphertext = encrypt(models.CharField(max_length=1000)) # Required
-    url = encrypt(models.CharField(max_length=64, blank=True))
-    tags = encrypt(models.CharField(max_length=10, choices=pw_tags, null=True, blank=True))
-    comment = encrypt(models.CharField(max_length=1000, null=True, blank=True))
+    name = models.CharField(max_length=64) # Required
+    username = models.CharField(max_length=1000, null=True, blank=True)
+    ciphertext = encrypt(models.CharField(max_length=1000)) # Required, encrypted
+    url = models.CharField(max_length=64, blank=True)
+    tags = models.CharField(max_length=10, choices=pw_tags, null=True, blank=True)
+    comment = encrypt(models.CharField(max_length=1000, null=True, blank=True)) # Encrypted
     def __str__(self):
         return f"{self.user} ({self.name})"
